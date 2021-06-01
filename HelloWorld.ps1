237d4331ae2c95679c224b2b59b3a3c99da8c8b2
Alessandro Santerini <alessandro.santerini@gmail.com>
2021-06-01 11:06:49 +0200

    nuovo file aggiunto

---
+++ HelloWorld.ps1
@@ -0,0 +1,45 @@
+# Doc https://docs.microsoft.com/en-us/powershell/scripting/overview?view=powershell-7.1
+
+$a = "Sono una stringa"
+$n = 2
+
+Write-Host "La variable n contiene $n"
+Write-Host 'La variable n contiene $n'
+
+Write-Host "Questa è una stringa con espressione $(2+2)"
+
+$s = "Una stringa con HTML
+<div id=""ciao""></div>
+"
+
+$a = (1, 2, 3, 4)
+
+$a.Add(2) # Non si può fare
+
+$a += 2
+
+foreach ($e in $a) {
+    if (($e % 2) -eq 0) {
+        Write-Output "Il numero è $e" # >> outfile.txt
+    }
+}
+
+foreach ($n in 0 .. 100) {
+    Write-Host "Installing server base-$n.unipi.it"
+}
+
+Get-NetAdapter -Name vEth*
+
+function Get-Pippo ($n) {
+    Write-Output "Pippo $n"
+}
+
+Get-Pippo -n 2
+
+Get-Disk | select FriendlyName, SerialNumber | Out-GridView
+
+Invoke-WebRequest http://www.unipi.it
+
+Get-NetAdapter
+
+Get-PackageProvider
