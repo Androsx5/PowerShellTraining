8713c07ddf5ea17e57ba602c0521ee6a73282768
Alessandro Santerini <alessandro.santerini@gmail.com>
2021-06-14 13:38:48 +0200

    completo con invio mail

--- Speedtestbycli.ps1
+++ Speedtestbycli.ps1
@@ -28,8 +28,9 @@ Set-Location -Path $pathspeedtestexe
 # estraggo ora dalla data
 [int]$hour = Get-Date -Format HH
 
+
 # dalle 8 alle 20 faccio speedtest ogni ora
-while($hour -lt 21 -and  $hour -gt 7) {
+while($hour -lt 21 -and  $hour -gt 7 -and $min -eq 00 -and $seconds -eq 00) {
 
     # Invoke-Expression -Command $exetorun # > test.txt
     Add-Content -Path $speedtestreport -Value "-----------------------------------------------------------------------------------"
@@ -39,21 +40,44 @@ while($hour -lt 21 -and  $hour -gt 7) {
 
     Start-Process -FilePath $pathspeedtestexe$exetorun -ArgumentList $server_vodafone_milano -Wait -WindowStyle Hidden -RedirectStandardOutput $temp
     Add-Content -Path $speedtestreport -Value (Get-Content -Path $temp)
+
+    # estraggo ora dalla data
+    [int]$hour = Get-Date -Format HH
+
+    [int]$min = Get-Date -Format mm
+
+    [int]$seconds = Get-Date -Format ss
  
 }
 
+
+# preparo invio report per mail prima di uscire dallo script
+$uid = "info@alessandrosanterini.it"
+$pwd = "01000000d08c9ddf0115d1118c7a00c04fc297eb01000000e00df9985181374ba4d072447aa97ecc000000000200000000001066000000010000200000008bfdd65accf3466fa9425509a7793b066b6fa1931d7fe623de072ba7317471d9000000000e8000000002000020000000a6f0d01c319afd58bdb883d83a8dd60ec99a55700b990eb34b62c71804eadd9c20000000190b6c70aaa0cf188aa3a242f3db39fcbf8dbf60b6117c8db735d3edbc33250f400000005a6d1ee88b6ae88972f6146e6b9d67599f12e610ede831938b2966967c7d027bc0079bfa738cb72e49c6734177e823858dfca21d1726a2fcc31012b468623c6f"
+
+$spwd = ConvertTo-SecureString $pwd
+$Cred = New-Object System.Management.Automation.PSCredential ($uid,$spwd)
+
+#PSCredential Object al volo
+$sendMailParams = @{
+    From = 'speedtestmonitor@speedtest.it' 
+    To = 'alessandro.santerini@unipi.it'
+    Subject = "Report Speedtest $(Get-Date -Format dd)/$(Get-Date -Format MM)/$(Get-Date -Format yyyy)"
+    Body = 'Report Speedtest in allegato'
+    SMTPServer = 'smtp.alessandrosanterini.it'
+    Credential = $Cred
+    Attachments = $speedtestreport
+}
+
+Send-MailMessage @sendMailParams
+
+# sleep -Milliseconds 5000
 
 # cancello dopo aver inviato per mail -pulizia
 Remove-Item -Path $pathspeedtestexe$speedtestreport
 Remove-Item -Path $pathspeedtestexe$temp
 
 
-
-
-
-
-
-# Send-MailMessage -To “Manager 1 <alessandro.santerini@gmail.com>" -From “Reports Admin <Reportadmin@xyx.com>" -SMTPServer smtp.gmail.com -Subject “Daily report” -Body “This is a daily report of server uptime”
 
 
 
