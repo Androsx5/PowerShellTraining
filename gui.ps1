bdfd245c0ed1fdd831aa0c805460d32e729145ad
Alessandro Santerini <alessandro.santerini@gmail.com>
2021-07-01 16:19:12 +0200

    aggiunti colori alle TextBox

--- gui.ps1
+++ gui.ps1
@@ -1,3 +1,36 @@
+﻿##[Ps1 To Exe]
+##
+##Kd3HDZOFADWE8uK1
+##Nc3NCtDXThU=
+##Kd3HFJGZHWLWoLaVvnQnhQ==
+##LM/RF4eFHHGZ7/K1
+##K8rLFtDXTiW5
+##OsHQCZGeTiiZ4tI=
+##OcrLFtDXTiW5
+##LM/BD5WYTiiZ4tI=
+##McvWDJ+OTiiZ4tI=
+##OMvOC56PFnzN8u+Vs1Q=
+##M9jHFoeYB2Hc8u+Vs1Q=
+##PdrWFpmIG2HcofKIo2QX
+##OMfRFJyLFzWE8uK1
+##KsfMAp/KUzWJ0g==
+##OsfOAYaPHGbQvbyVvnQX
+##LNzNAIWJGmPcoKHc7Do3uAuO
+##LNzNAIWJGnvYv7eVvnQX
+##M9zLA5mED3nfu77Q7TV64AuzAgg=
+##NcDWAYKED3nfu77Q7TV64AuzAgg=
+##OMvRB4KDHmHQvbyVvnQX
+##P8HPFJGEFzWE8tI=
+##KNzDAJWHD2fS8u+Vgw==
+##P8HSHYKDCX3N8u+Vgw==
+##LNzLEpGeC3fMu77Ro2k3hQ==
+##L97HB5mLAnfMu77Ro2k3hQ==
+##P8HPCZWEGmaZ7/K1
+##L8/UAdDXTlaDjofG5iZk2WqgcUkFWuCpkJiO+K6y+s7+sjPcCddUfFp+gCzuF0KpQLIdU7UlvcIQaTkvPPkf7aLsHPKgeqwfnfVwJeCWo9I=
+##Kc/BRM3KXhU=
+##
+##
+##fd6a9f26a06ea3bc99616d4851b372ba
 <#
 
 .SYNOPSIS
@@ -26,15 +59,8 @@ $Form = New-Object system.Windows.Forms.Form
 $Form.Text = "Network Info"
 $Form.TopMost = $true
 $Form.Width = 500
-$Form.Height = 500
+$Form.Height = 630
 
-$textBoxEth = New-Object system.windows.Forms.TextBox
-$textBoxEth.Width = 200
-$textBoxEth.Height = 200
-$textBoxEth.location = new-object system.drawing.point(200,100)
-$textBoxEth.AppendText($ethernet_ip.ToString())
-$textBoxEth.ReadOnly = $true
-$Form.controls.Add($textBoxEth)
 
 $labeleth = New-Object system.windows.Forms.Label
 $labeleth.Width = 100
@@ -42,6 +68,21 @@ $labeleth.Height = 100
 $labeleth.location = new-object system.drawing.point(100,100)
 $labeleth.Text = "Ethernet IP: "
 $Form.controls.Add($labeleth)
+
+$textBoxEth = New-Object system.windows.Forms.TextBox
+$textBoxEth.Width = 200
+$textBoxEth.Height = 200
+$textBoxEth.location = new-object system.drawing.point(200,100)
+$textBoxEth.BackColor = "Red"
+if ($ethernet_ip) { 
+    
+    $textBoxEth.AppendText($ethernet_ip.ToString())
+    $textBoxEth.BackColor = "#FF8FBC8B" 
+
+}
+$textBoxEth.ReadOnly = $true
+$Form.controls.Add($textBoxEth)
+
 
 
 $labelwifi = New-Object system.windows.Forms.Label
@@ -56,14 +97,62 @@ $textBoxWiFi = New-Object system.windows.Forms.TextBox
 $textBoxWiFi.Width = 200
 $textBoxWiFi.Height = 200
 $textBoxWiFi.location = new-object system.drawing.point(200,200)
-if ($wifi_ip) { $textBoxWiFi.AppendText($wifi_ip.ToString()) }
+$textBoxWiFi.BackColor = "Red"
+if ($wifi_ip) { 
+    
+    $textBoxWiFi.AppendText($wifi_ip.ToString())
+    $textBoxWiFi.BackColor = "#FF8FBC8B"
+
+}
 $textBoxWiFi.ReadOnly = $true
 $Form.controls.Add($textBoxWiFi)
 
+$labelct = New-Object system.windows.Forms.Label
+$labelct.Width = 100
+$labelct.Height = 100
+$labelct.location = new-object system.drawing.point(100,300)
+$labelct.Text = "Connect Tunnel IP:"
+$Form.controls.Add($labelct)
+
+$textBoxCt = New-Object system.windows.Forms.TextBox
+$textBoxCt.Width = 200
+$textBoxCt.Height = 200
+$textBoxCt.location = new-object system.drawing.point(200,300)
+$textBoxCt.BackColor = "Red"
+if ($connect_tunnel_ip) { 
+    
+    $textBoxCt.AppendText($connect_tunnel_ip.ToString())
+    $textBoxCt.BackColor = "#FF8FBC8B" 
+
+}
+$textBoxCt.ReadOnly = $true
+$Form.controls.Add($textBoxCt)
+
+
+$labeleth2 = New-Object system.windows.Forms.Label
+$labeleth2.Width = 100
+$labeleth2.Height = 100
+$labeleth2.location = new-object system.drawing.point(100,400)
+$labeleth2.Text = "Ethernet 2 (Eventuale Adattatore) IP: "
+$Form.controls.Add($labeleth2)
+
+$textBoxeth2 = New-Object system.windows.Forms.TextBox
+$textBoxeth2.Width = 200
+$textBoxeth2.Height = 200
+$textBoxeth2.location = new-object system.drawing.point(200,400)
+$textBoxeth2.BackColor = "Red"
+if ($ethernet2_ip) { 
+    
+    $textBoxeth2.AppendText($ethernet2_ip.ToString())
+    $textBoxeth2.BackColor = "#FF8FBC8B" 
+
+}
+$textBoxeth2.ReadOnly = $true
+$Form.controls.Add($textBoxeth2)
 
 
 $okButton = New-Object System.Windows.Forms.Button
-$okButton.Location = New-Object System.Drawing.Point(300,300)
+$okButton.Location = New-Object System.Drawing.Point(350,500)
 $okButton.Text = 'OK'
 $okButton.Width = 100
 $okButton.Height = 60
@@ -72,6 +161,5 @@ $Form.AcceptButton = $okButton
 $Form.Controls.Add($okButton)
 
 
-
-$form.Add_Shown({$form.Activate()})    
-$dialogResult = $form.ShowDialog()
+$Form.Add_Shown({$Form.Activate()})    
+$dialogResult = $Form.ShowDialog()
